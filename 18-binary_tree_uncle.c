1e8cead4b4d891145ee16764b950cec4fb437123
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-09-01 00:30:10 +0100

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_uncle - Function that finds the uncle of a node
+ * binary_tree_uncle - Finds the uncle of a node.
  * @node: Pointer to the node to find the uncle.
- * Return: Returns pointer to the uncle node
+ * Return: Returns a pointer to the uncle node.
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
