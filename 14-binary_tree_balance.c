cf6e6d34c6467ae3eb9fe2c9de85bfce771d151a
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-09-01 00:25:39 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - function that measures the height of a binary tree
+ * binary_tree_height - Measures the height of a binary tree.
  * @tree: Pointer to the root node of the tree to measure the height.
- * Return: height of binary tree
+ * Return: Height of the binary tree.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
