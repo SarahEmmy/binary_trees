5df8378a06b658bb70e37908bf046e116f0d0945
SarahEmmy <bawahsarah@gmail.com>
2023-08-31 16:16:54 -0700

    updates

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,10 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - function that goes through a binary tree
- * using post-order traversal
- * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node.
- * Return: void - No return
+ * binary_tree_postorder - traverses a binary tree using post-order
+ * @tree: Pointer to root node
+ * @func: Pointer to function for each node
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - function that measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height.
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: Pointer to root node
  * Return: height of binary tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
