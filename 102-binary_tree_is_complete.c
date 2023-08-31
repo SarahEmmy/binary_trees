47e7c313a6b55fb481ad1bccb11e850ea8e716fd
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-09-01 00:34:37 +0100

    Update 102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
-* tree_size - measures the size of a binary tree
-* @tree: pointer to the root node of the tree to measure the size
-* Return: size of a binary tree
-* If tree is NULL, the function must return 0
-**/
+ * tree_size - Measures the size of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure the size.
+ * Return: Size of the binary tree. If tree is NULL, the function
+ *         must return 0.
+ */
 size_t tree_size(const binary_tree_t *tree)
 {
 	int count = 0;
@@ -19,12 +19,12 @@ size_t tree_size(const binary_tree_t *tree)
 }
 
 /**
-* is_complete - checks if a binary tree is complete
-* @tree: pointer to the root node of the tree to check
-* @pos: position of node
-* @nodes: size of the tree
-* Return: 1 is tree is perfect, 0 if not or tree is NULL
-**/
+ * is_complete - Checks if a binary tree is complete.
+ * @tree: Pointer to the root node of the tree to check.
+ * @pos: Position of the node.
+ * @nodes: Size of the tree.
+ * Return: 1 if tree is perfect, 0 if not or tree is NULL.
+ */
 int is_complete(binary_tree_t *tree, int pos, int nodes)
 {
 	int left, right;
